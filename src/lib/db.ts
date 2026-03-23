2104c6653489464f149778db32aa7aeb7661f546
emptyboy84 <emptyboy85@gmail.com>
2026-03-23 11:33:53 +0900

    let ->const

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -13,11 +13,11 @@ if (!process.env.MONGODB_URI) {//MONGODB_URI가 없으면
 //개발환경에서는 연결을 재사용하도록 설정합니다.
 
 if (process.env.NODE_ENV === "development") {//개발환경에서는 연결을 재사용  안하면 핫리로드할때마다 연결해서 느려짐
-   let globalWithMongo = global as typeof globalThis & {
+   const globalWithMongo = global as typeof globalThis & {
       _mongoClientPromise?: Promise<MongoClient>
    };//globalThis는 전역객체로 mongoClientPromise가 없어서 에러가 나는데 typeof globalThis로 타입을 지정해서 에러를 해결합니다.
 
-   if (!globalWithMongo._mongoClientPromise) {
+   if (!globalWithMongo._mongoClientPromise) {//_mongoClientPromise가 없으면
       client = new MongoClient(uri, options);//연결
       globalWithMongo._mongoClientPromise = client.connect();//연결
    }
